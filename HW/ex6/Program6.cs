a2eba415759c59e59fb5d3860f421b3c77bffb99
Key <arhiv.baev@gmail.com>
2023-01-26 09:33:15 +0300

    ex4

---
+++ HW/ex6/Program6.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write (userNumber1 + " -> " );
+
+if (userNumber1 % 2 == 0)
+{
+    Console.Write ("да");
+}
+else
+    Console.Write ("нет");   
+   
+
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
