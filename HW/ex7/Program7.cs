06d7255c6ff43395aedcaa94b5caa18527390d39
Key <arhiv.baev@gmail.com>
2023-01-26 10:41:56 +0300

    ex8

---
+++ HW/ex7/Program7.cs
@@ -0,0 +1,25 @@
+﻿Console.Write ("Введите число N: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+int a = 1;
+
+Console.Write (userNumber1 + " -> ");
+
+while (a <= userNumber1)
+{
+    if (a % 2 == 0)
+    {
+    Console.Write (a);
+    }
+    a++;
+    if ((a-1) % 2 == 0)
+    {   
+        if (a < userNumber1)
+        {Console.Write (", ");}
+    }
+}
+
+
+ 
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
