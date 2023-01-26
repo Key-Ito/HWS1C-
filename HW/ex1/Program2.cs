0608445a66e181e88c5e0a80875157deadd7abf1
Key <arhiv.baev@gmail.com>
2023-01-26 09:25:20 +0300

    ex2,3

---
+++ HW/ex1/Program2.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write ("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+
+Console.Write ("a = " + userNumber1 + ", b = " + userNumber2 + " -> max = ");
+
+if (userNumber1 > userNumber2)
+{
+    Console.Write (userNumber1);   
+}
+else
+    {
+    Console.Write (userNumber2);
+    }
+
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
---
+++ HW/ex4/Program4.cs
@@ -0,0 +1,35 @@
+﻿Console.Write ("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+Console.Write ("Введите тертье целое число: ");
+string userInput3 = Console.ReadLine() ?? "";
+
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+int userNumber3 = int.Parse(userInput3);
+
+Console.Write (userNumber1 + ", " + userNumber2 + ", " + userNumber3 + " -> ");
+
+if (userNumber1 > userNumber2)
+{
+    if (userNumber1 > userNumber3)
+    {
+        Console.Write (userNumber1);
+    }
+    else
+        Console.Write (userNumber3);   
+}
+else
+    
+        if (userNumber2 > userNumber3)
+    {
+        Console.Write (userNumber2);
+    }
+    else
+        Console.Write (userNumber3);
+    
+
+//Console.WriteLine(userNumber + " -> " + userNumber * userNumber);
